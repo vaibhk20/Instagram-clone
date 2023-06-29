51ed5a4c38421fd9e819ec8022599ddf42c4aa50
Vaibhavi Kolloju <vaibhavik@geekyants.com>
2023-06-29 18:27:06 +0530

    initial commit

---
+++ src/components/ Carousel/carousel.styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  post: {
+    width: "100%",
+    maxHeight: "700px",
+  },
+  carouselImage: {
+    objectFit: "contain",
+    aspectRatio: "1/1",
+  },
+});
+
+export default useStyles;
